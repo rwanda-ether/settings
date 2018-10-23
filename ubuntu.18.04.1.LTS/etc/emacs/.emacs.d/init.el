a48e0bcaa78e3d9f5494149b017de6219ffa4d0b
rwanda-ether <rwanda-ether@localhost>
2018-10-23 12:24:40 +0900

    In order to write about life, first you must live it! -- Ernest Hemingway

--- ubuntu.18.04.1.LTS/etc/emacs/.emacs.d/init.el
+++ ubuntu.18.04.1.LTS/etc/emacs/.emacs.d/init.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-10-23 12:06:40 hamada>
+;; Time-stamp: <2018-10-23 12:23:29 hamada>
 
 (require 'mozc)
 (set-language-environment "Japanese")
@@ -12,3 +12,21 @@
 
 ;; --- display time ---
 (display-time)
+
+;; --- scroll-step --- 
+(setq scroll-step 1)
+
+;; --- save place ---
+(require 'saveplace)
+(setq-default save-place t)
+
+;; --- disable the startup message ---
+(setq inhibit-startup-message t)
+
+;; --- for CUDA ---
+(setq auto-mode-alist
+      (cons (cons "\\.cu$" 'c++-mode) auto-mode-alist))
+
+;; --- tab width ---
+(setq-default tab-width 4)
+
